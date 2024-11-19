f8f1936127c9bebb4861794610264cb2e6dff6b4
mohammadsweiti <mohammadsweity57669@gmail.com>
2024-11-19 13:55:34 +0200

    code

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -72,3 +72,74 @@ console.log(w);
 const bb = [44,55];
 const cc = bb.concat(aa);
 console.log(cc);
+
+///////////////////////////////////.................................
+
+function sum(n1,n2){
+    return n1+n2;
+}
+console.log(sum(5,8));
+
+//document.write(sum(3,13));
+// document.write( " this is the new file")
+
+sum(3,4);
+
+/*const sum = function(n1,n2){
+    return n1+n2;
+}*/
+
+const arrow1 = (n1,n2)=>{
+    return n1+n2;
+}
+
+console.log(arrow1(3,4));
+
+const square = n1=> n1*n1;
+
+console.log(square(6));
+
+
+const mohd = n => n*n*n*n;
+
+console.log(mohd(2));
+
+
+const what = (name1 , infor) => {
+    return "this is the " +name1  + "and this give me this "+ infor+"   ";
+}
+
+console.log(what("mohammad ","comp"));
+
+const printhellow = () => console.log("this is the print function");
+
+printhellow();
+printhellow();
+printhellow();
+printhellow();
+
+
+const arrr = [10,20,30,40,50];
+
+const addbouns = (mark)=>{
+    if (mark+5 <= 45){
+        return mark+5;
+    }
+    else return mark;
+
+}
+
+const arrr1 = arrr.map(addbouns);
+console.log(arrr);
+console.log(arrr1);
+
+const arrr4 = arrr1.map((mark,index)=> (mark+5) < 45? mark+5+index:mark);
+console.log(arrr4);
+
+
+// const aaaa = arrr4.map()
+
+const names = ["mohd","ahmad","ali"];
+
+const newnames = names.map((name, index) => `${index} : ${name.length}` );
+console.log(newnames);
